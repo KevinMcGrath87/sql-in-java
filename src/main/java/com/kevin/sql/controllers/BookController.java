0857b7f40690a3169bc2b87fcb7950a6d5ad12a6
Kevin McGrath <kpmcgra@gmail.com>
2022-12-05 19:37:01 -0600

    added jsp views and used @controller to render html to client.

---
+++ src/main/java/com/kevin/sql/controllers/BookController.java
@@ -0,0 +1,26 @@
+package com.kevin.sql.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.kevin.sql.models.Book;
+import com.kevin.sql.services.BookService;
+
+@Controller
+public class BookController {
+	private final BookService bookService;
+	
+	public BookController(BookService bookService) {
+		this.bookService = bookService;
+	}
+	
+	@GetMapping("/books/{id}")
+	public String index(Model model, @PathVariable("id") Long id) {
+		Book book = bookService.findBook(id);
+		model.addAttribute("book", book);
+		return "index.jsp";
+	}
+
+}
--- src/main/webapp/WEB-INF/index.jsp
+++ src/main/webapp/WEB-INF/index.jsp
@@ -8,6 +8,9 @@
 <title>Insert title here</title>
 </head>
 <body>
+<h1>Title:<c:out value ="${book.getTitle()}"></c:out></h1>
+<h1>Description:<c:out value = "${book.getDescription()}"></c:out></h1>
+<h1>Number of Pages:<c:out value = "${book.getNumberOfPages()}"></c:out></h1>
 
 </body>
 </html>
